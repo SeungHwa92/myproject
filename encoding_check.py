d5874a375d4ea26172fe7e8611da08a454d29317
SeungHwa92 <sud0303@naver.com>
2019-01-01 15:40:53 +0900

    qqqq

--- encoding_check.py
+++ encoding_check.py
@@ -2,12 +2,15 @@ import torch
 from torch import nn
 import encoding
 
+
+import encoding.nn.SyncBatchNorm as BN
+
 class network(nn.Module):
     def __init__(self):
         super(network, self).__init__()
         self.fc = nn.Linear(10, 10)
         self.bn = encoding.nn.BatchNorm1d(10)
-        #self.bn = nn.BatchNorm1d(10)
+        #self.bn = BN(10)
     def forward(self, x):
         x = self.fc(x)
         x = self.bn(x)
@@ -18,7 +21,7 @@ net = network()
 
 net.cuda()
 net = nn.DataParallel(net)
-encoding.parallel.patch_replication_callback(net)
+
 
 import numpy as np
 a = np.random.random(100).reshape((10, 10))
