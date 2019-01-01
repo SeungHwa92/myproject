fabc185a9bb9efd1d958714850793433d8d1d33d
root <root@titan.q4>
2019-01-01 06:50:40 +0000

    Modify the torch-encoding

--- encoding_check.py
+++ encoding_check.py
@@ -3,13 +3,11 @@ from torch import nn
 import encoding
 
 
-import encoding.nn.SyncBatchNorm as BN
-
 class network(nn.Module):
     def __init__(self):
         super(network, self).__init__()
-        self.fc = nn.Linear(10, 10)
-        self.bn = encoding.nn.BatchNorm1d(10)
+        self.fc = nn.Linear(1000, 1000)
+        self.bn = encoding.nn.SyncBatchNorm(1000)
         #self.bn = BN(10)
     def forward(self, x):
         x = self.fc(x)
@@ -24,8 +22,10 @@ net = nn.DataParallel(net)
 
 
 import numpy as np
-a = np.random.random(100).reshape((10, 10))
+a = np.random.random(1000000).reshape((1000, 1000))
 a = torch.Tensor(a)
-out = net(a)
+
+for i in range(100000):
+    out = net(a)
 
 print(out)
